93f5f59ac6157fdec63ebbf192225f4f4ea112d7
kevinkutoyi <kevinkutoyi95@gmail.com>
2022-08-26 14:21:09 +0100

    deploy

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return ( 
     <div className="hero">
     <div className="card bg-dark text-white border-0">
-    <img className="card-img" src="/assets/bg.jpg" alt="background" height="550px"/>
+    <img className="card-img" src="/public/assets/bg.jpg" alt="background" height="550px"/>
     <div className="card-img-overlay d-flex flex-column justify-content-center">
         <div className="container">
         <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASONS ARRIVALS</h5>
